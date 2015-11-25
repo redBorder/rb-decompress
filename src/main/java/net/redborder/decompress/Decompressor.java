186ce071c5c3ee307dd6bdfde896a051ee7c556b
Andrés Gomez <agomez@redborder.net>
2015-11-25 13:04:52 +0100

    Remove unused import.

--- src/main/java/net/redborder/decompress/Decompressor.java
+++ src/main/java/net/redborder/decompress/Decompressor.java
@@ -1,14 +1,10 @@
 package net.redborder.decompress;
 
 import net.redborder.apache.commons.compress.PasswordRequiredException;
-import net.redborder.decompress.exceptions.PasswordProtectedException;
 import net.redborder.decompress.models.ArchiveFile;
 
 import java.util.List;
 
-/**
- * Created by fernando on 06/11/15.
- */
 public interface Decompressor {
 
     public List<ArchiveFile> decompress() throws PasswordRequiredException;
